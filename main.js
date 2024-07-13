a8ff443fc8e9a040e45024612fa3c5d853a4ab2d
OpeltsUnlimited <patrick@opelt.co.at>
2024-07-13 20:48:51 +0200

    adding infos about hw to run stuff

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
+// actevate node version "nvm use"
+// new project "npm init"
+// run program "npm start"  <- setrup in "package.json"
+// run test "npm test"  <- setrup in "package.json"
+
 console.log('hello world')
