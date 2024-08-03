3617d1490056d103e7e14df2effe210cfa12f4ab
OpeltsUnlimited <patrick@opelt.co.at>
2024-08-03 12:05:07 +0200

    A_5 Main

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 // actevate node version "nvm use"
 // new project "npm init"
 // run program "npm start"  <- setrup in "package.json"
+//  npm run start BASE_URL
 // run test "npm test"  <- setrup in "package.json"
 
-console.log('hello world')
+function main() {
+    if(process.argv.length < 1) {
+        return
+    }
+    console.log("nodepath:", process.argv[0])
+    if(process.argv.length < 2) {
+        return
+    }
+    console.log("this file:", process.argv[1])
+    if(process.argv.length < 3) {
+        console.log("NO URL PROVIDED")
+        return
+    }
+    const baseURL = process.argv[2]
+    console.log(`starting processing of: ${baseURL}...`)
+}
+  
+main()
+  
